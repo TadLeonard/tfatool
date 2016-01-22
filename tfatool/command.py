26e7179a01baa53959f2f65fa3519985fa2a0d8c
Tad Leonard <tadfleonard@gmail.com>
2016-01-21 21:59:08 -0600

    separated out command.py request construction for unit testing

--- tfatool/command.py
+++ tfatool/command.py
@@ -8,34 +8,38 @@ from .info import URL, DEFAULT_DIR
 logger = logging.getLogger(__name__)
 
 
-class Op(IntEnum):
+class Operation(IntEnum):
     list_files = 100
     count_files = 101
 
 
-def get(op: Op, url=URL, **params):
-    params.update(op=int(op))
-    return cgi.get(cgi.Entrypoint.command, url=url, **params)
+def _get(operation: Operation, url=URL, **params):
+    """HTTP GET of the FlashAir command.cgi entrypoint"""
+    prepped_request = _prep_get(operation, url=url, **params)
+    return cgi.send(prepped_request)
+
+
+def _prep_get(operation: Operation, url=URL, **params):
+    params.update(op=int(operation))  # op param required
+    return cgi.prep_get(cgi.Entrypoint.command, url=url, **params)
 
 
 ##################
 # command.cgi API
 
-
 def list_files(*filters, remote_dir=DEFAULT_DIR, url=URL):
-    response = get(Op.list_files, url, DIR=remote_dir)
+    response = _get(Operation.list_files, url, DIR=remote_dir)
     files = _split_file_list(response.text)
     return (f for f in files if all(filt(f) for filt in filters))
 
 
 def count_files(remote_dir=DEFAULT_DIR, url=URL):
-    response = get(Op.count_files, url, DIR=remote_dir)
+    response = _get(Operation.count_files, url, DIR=remote_dir)
     return int(response.text)
 
 
 #####################
 # API implementation
-
 
 _fields = ["directory", "filename", "size", "attribute", "date", "time"]
 FileInfo = namedtuple("FileInfo", _fields)
