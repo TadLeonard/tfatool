a6f4d3cd73dc288b46bf963d208f2891fdf32fd2
Tad Leonard <tadfleonard@gmail.com>
2016-01-21 21:59:43 -0600

    finished splitting of request prepping / sending in CGI module

--- tfatool/cgi.py
+++ tfatool/cgi.py
@@ -22,25 +22,31 @@ class Entrypoint(str, Enum):
 # If we just use requests.get and requests.post functions,
 # the URL construction is done behind the scenes and we can't test it
 
-def prepare_request(method, entrypoint, url=URL, req_kwargs=None, **params):
+def request(method, entrypoint: Entrypoint, url=URL,
+            req_kwargs=None, send_kwargs=None, **params):
+    prepared_req = prep_request(method, entrypoint, url=url,
+                                req_kwargs=req_kwargs, **params)
+    send_kwargs = send_kwargs or {}
+    return send(prepared_request, **send_kwargs)
+
+
+def prep_request(method, entrypoint, url=URL, req_kwargs=None, **params):
     resource = urljoin(url, entrypoint)
     req_kwargs = req_kwargs or {}
-    request = requests.Request(resource, params=params, **req_kwargs)
-    prepped = session.prepare_request(reqeust)
+    request = requests.Request(method, resource, params=params, **req_kwargs)
+    prepped = session.prepare_request(request)
     logger.info("Request: {}".format(prepped.url))
     return prepped
 
 
-def request(method, entrypoint, url=URL,
-            req_kwargs=None, send_kwargs=None, **params):
-    prepared_req = prepare_request(method, entrypoint, url=url,
-                                   req_kwargs=req_kwargs, **params)
-    send_kwargs = send_kwargs or {}
-    response = session.send(prepared_request, **send_kwargs)
+def send(prepped_request, **send_kwargs):
+    response = session.send(prepped_request, **send_kwargs)
     logger.debug("Response: {}".format(response))
     return response
 
 
+prep_get = partial(prep_request, "GET")
 get = partial(request, "GET")
+prep_post = partial(prep_request, "POST")
 post = partial(request, "POST")
 
