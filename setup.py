039f166110b4d8822affac4690fd189ed2324edf
Tad Leonard <tadfleonard@gmail.com>
2016-01-22 19:19:37 -0600

    preparing for 1.2.0 release
    made setup.py a little more DRY

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@ from setuptools import setup
 import tfatool._version
 
 
+version = tfatool._version.__version__
+url = "https://github.com/TadLeonard/tfatool"
+download = "{}/archive/{}.tar.gz".format(url, version)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name="tfatool",
       version=tfatool._version.__version__,
       scripts=["flashair-util"],
@@ -10,10 +19,11 @@ setup(name="tfatool",
       packages=["tfatool"],
       description=("Tools for syncing files with the "
                    "Toshiba FlashAir wireless SD card"),
+      long_description=read("README.md"),
       author="Tad Leonard",
       author_email="tadfleonard@gmail.com",
       keywords="wireless sd card sync toshiba flashair", 
-      url="https://github.com/TadLeonard/tfatool",
-      download_url="https://github.com/TadLeonard/tfatool/archive/v1.1.0.tar.gz",
+      url=url,
+      download_url=download,
 )
 
